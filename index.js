92c1ca67f3fee51a02f1f816ae883febbc70c02f
Joshua Andujar <andujar0218@gmail.com>
2019-07-18 02:30:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function getCart() {
 }
 
 function setCart(c) {
-  cart = c;
+  cart = null;
   return cart;
 }
 
@@ -64,5 +64,11 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+    if(cardNumber == null) {
+        return "Sorry, we don't have a credit card on file for you.";
+    } else {
+        var orderTotal = total();
+        setCart();
+        return `Your total cost is $${orderTotal}, which will be charged to the card ${cardNumber}.`;
+    }
 }
